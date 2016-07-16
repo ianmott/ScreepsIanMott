f4bbdcc46fb168a8cc3c6afc5979511e7d667fe6
ianmott <ian@derivedalgorithm.com>
2016-07-16 18:16:16 -0400

    u

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 /** @module main */
 
-//require('./ext_Array');
-//require('./ext_Creep');
-//require('./ext_Object');
-//require('./ext_Room');
 require('./ext_StructureTower');
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
@@ -12,23 +8,25 @@ var rolerepairer = require('role.repairer');
 var roleguard = require('role.guard');
 
 let controller_tower = require('./controller_tower');
-var TickCounter = 10000;
+
 module.exports.loop = function() {
     for (let name in Memory.creeps) { if (!Game.creeps[name]) { delete Memory.creeps[name]; } }
     
     controller_tower.run();
 	
 	// Variables
-	var energyspawnlevel = 275;
+	var energyspawnlevel = 299;
 	var NumofTypes = 5;
+	var toggle = false;
     var Types = new Array(NumofTypes);           // Type Name     Type Min       Type Current
-    Types[0] = new Array(3); Types[0][0] = 'harvester'; Types[0][1] = 5; Types[0][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester').length;
-    Types[1] = new Array(3); Types[1][0] = 'builder';   Types[1][1] = 3; Types[1][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder').length;
-    Types[2] = new Array(3); Types[2][0] = 'upgrader';  Types[2][1] = 8; Types[2][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader').length;
-    Types[3] = new Array(3); Types[3][0] = 'repairer';  Types[3][1] = 2; Types[3][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer').length;
+    Types[0] = new Array(3); Types[0][0] = 'harvester'; Types[0][1] = 3; Types[0][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester').length;
+    Types[1] = new Array(3); Types[1][0] = 'builder';   Types[1][1] = 2; Types[1][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder').length;
+    Types[2] = new Array(3); Types[2][0] = 'upgrader';  Types[2][1] = 6; Types[2][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader').length;
+    Types[3] = new Array(3); Types[3][0] = 'repairer';  Types[3][1] = 4; Types[3][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer').length;
     Types[4] = new Array(3); Types[4][0] = 'guard';  Types[4][1] = 2; Types[4][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'guard').length;
+    
     for (i=0; i <NumofTypes; i++){
-        if (Types[i][1] > Types[i][2] && Game.spawns.Spawn1.energy > energyspawnlevel)
+        if (Types[i][1] > Types[i][2] && (Game.spawns.Spawn1.room.energyAvailable) > energyspawnlevel && !toggle)
         {
             if (Types[i][0] == 'guard'){ 
                 var newName = Game.spawns.Spawn1.createCreep([TOUGH,ATTACK,ATTACK,MOVE], undefined, {role: Types[i][0]});
@@ -37,13 +35,9 @@ module.exports.loop = function() {
                 var newName = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: Types[i][0]});
             }
             console.log('Spawning new '+Types[i][0]+': ' + newName);
-            TickCounter= 10001;
+            toggle = true;
         }
-        
     }
-    if (TickCounter%100 == 0){ console.log('h#/X='+Types[0][2]+'/'+Types[0][1]+' b#/X='+Types[1][2]+'/'+Types[1][1]+' u#/X='+Types[2][2]+'/'+Types[2][1]+' r#/X='+Types[3][2]+'/'+Types[3][1]+' g#/X='+Types[4][2]+'/'+Types[4][1]);
-    }
-    
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
@@ -62,9 +56,7 @@ module.exports.loop = function() {
             roleguard.run(creep);
         }
     }
-    if (TickCounter == 0) {TickCounter = 10000;} else {TickCounter--;}
+    if (Game.time%10==0){ console.log('Energy:'+Game.spawns.Spawn1.room.energyAvailable+' h#/X='+Types[0][2]+'/'+Types[0][1]+' b#/X='+Types[1][2]+'/'+Types[1][1]+' u#/X='+Types[2][2]+'/'+Types[2][1]+' r#/X='+Types[3][2]+'/'+Types[3][1]+' g#/X='+Types[4][2]+'/'+Types[4][1]);
+    }
+    //console.log(Game.time);
 };
-
-//G = require('./G');
-//W = require('./W');
-//util = require('./util');
