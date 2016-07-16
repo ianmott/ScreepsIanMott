ec4203c4841266963d73bbef0a22c8b308f7c6a2
ianmott <ian@derivedalgorithm.com>
2016-07-16 14:26:49 -0400

    u

--- main.js
+++ main.js
@@ -23,7 +23,8 @@ module.exports.loop = function() {
     controller_harv.run();
     // builder.run();
 	
-	var energyspawnlevel = 250;
+	// Variables
+	var energyspawnlevel = 299;
     var minharvester = 5;
 	var minbuilder = 6;
 	var minupgrader = 6;
@@ -32,27 +33,23 @@ module.exports.loop = function() {
     var builder = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
     var upgrader = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     var repairer = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
-    // console.log('h#/X='+harvester.length+'/'+minharvester+' b#/X='+builder.length+'/'+minbuilder+' u#/X='+upgrader.length+'/'+minupgrader+' r#/X='+repairer.length+'/'+minrepairer);
-    if(harvester.length < minharvester && Game.spawns.Spawn1.energy > energyspawnlevel) {
-        var newName = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'harvester'});
-        console.log('h#/X='+harvester.length+'/'+minharvester+' b#/X='+builder.length+'/'+minbuilder+' u#/X='+upgrader.length+'/'+minupgrader+' r#/X='+repairer.length+'/'+minrepairer);
-        console.log('Spawning new harvester: ' + newName);
-    }else
-    if(upgrader.length < minupgrader && Game.spawns.Spawn1.energy > energyspawnlevel) {
-        var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'upgrader'});
-        console.log('h#/X='+harvester.length+'/'+minharvester+' b#/X='+builder.length+'/'+minbuilder+' u#/X='+upgrader.length+'/'+minupgrader+' r#/X='+repairer.length+'/'+minrepairer);
-        console.log('Spawning new Upgrader: ' + newName);
-    }else
-    if(builder.length < minbuilder && Game.spawns.Spawn1.energy > energyspawnlevel) {
-        var newName = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'builder'});
-        console.log('h#/X='+harvester.length+'/'+minharvester+' b#/X='+builder.length+'/'+minbuilder+' u#/X='+upgrader.length+'/'+minupgrader+' r#/X='+repairer.length+'/'+minrepairer);
-        console.log('Spawning new builder: ' + newName);
-    }else
-    if(repairer.length < minrepairer && Game.spawns.Spawn1.energy > energyspawnlevel) {
-        var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'repairer'});
-        console.log('h#/X='+harvester.length+'/'+minharvester+' b#/X='+builder.length+'/'+minbuilder+' u#/X='+upgrader.length+'/'+minupgrader+' r#/X='+repairer.length+'/'+minrepairer);
-        console.log('Spawning new repairer: ' + newName);
+    
+    var Types = new Array(4);
+    Types[0] = new Array(3); Types[0][0] = 'harvester';Types[0][1] = 5;Types[0][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester').length;
+    Types[1] = new Array(3); Types[1][0] = 'builder';Types[1][1] = 6;Types[1][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder').length;
+    Types[2] = new Array(3); Types[2][0] = 'upgrader';Types[2][1] = 6;Types[2][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader').length;
+    Types[3] = new Array(3); Types[3][0] = 'repairer'; Types[3][1] = 2; Types[3][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer').length;
+    for (i=0; i <4; i++){
+        if (Types[i][1] < Types[i][2])
+        {
+            var newName = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: Types[i][0]});
+            console.log('h#/X='+Types[0][1]+'/'+Types[0][2]+' b#/X='+Types[1][1]+'/'+Types[1][2]+' u#/X='+Types[2][1]+'/'+Types[2][2]+' r#/X='+Types[3][1]+'/'+Types[3][2]);
+            console.log('Spawning new '+Types[i][0]+': ' + newName);
+        }
+        
     }
+    // console.log('h#/X='+harvester.length+'/'+minharvester+' b#/X='+builder.length+'/'+minbuilder+' u#/X='+upgrader.length+'/'+minupgrader+' r#/X='+repairer.length+'/'+minrepairer);
+    
     
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
