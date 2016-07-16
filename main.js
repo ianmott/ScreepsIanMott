70c20da71580b9ba25ef7cb233d0378aa0d4429c
ianmott <ian@derivedalgorithm.com>
2016-07-15 23:13:39 -0400

    u

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ module.exports.loop = function () {
     var builder = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
     var upgrader = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     var repairer = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
-    // console.log('h#/X='+harvester.length+'/'+minbuilder+' b#/X='+builder.length+'/'+minharvester+' u#/X='+upgrader.length+'/'+minupgrader+' r#/X='+repairer.length+'/'+minrepairer);
+     console.log('h#/X='+harvester.length+'/'+minbuilder+' b#/X='+builder.length+'/'+minharvester+' u#/X='+upgrader.length+'/'+minupgrader+' r#/X='+repairer.length+'/'+minrepairer);
     if(harvester.length < minharvester && Game.spawns.Spawn1.energy > 299) {
         var newName = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
@@ -28,7 +28,13 @@ module.exports.loop = function () {
     if(repairer.length < minrepairer && Game.spawns.Spawn1.energy > 200) {
         var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE], undefined, {role: 'repairer'});
         console.log('Spawning new repairer: ' + newName);
-    }   
+    }
+    //default
+    else
+    if(upgrader.length < minupgrader && Game.spawns.Spawn1.energy > 299) {
+        var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'upgrader'});
+        console.log('Spawning new Upgrader: ' + newName);
+    }
     
     var tower = Game.getObjectById('TOWER_ID');
     if(tower) {
