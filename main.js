f1ee150d81841eb7381ef20cd387a3ca17753618
ianmott <ian@derivedalgorithm.com>
2016-07-15 20:15:36 -0400

    Revert "u"
    This reverts commit 01477928dba459fa224058c0f613e4245d0f66cc.

--- main.js
+++ main.js
@@ -4,11 +4,6 @@ var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
 
-    if(_.filter(Game.creeps, (creep) => creep.memory.role == 'harvester').length < 4 && Game.spawns.Spawn1.energy > 200) {
-        var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
-        console.log('Spawning new harvester: ' + newName);
-    }
-    
     var tower = Game.getObjectById('TOWER_ID');
     if(tower) {
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
