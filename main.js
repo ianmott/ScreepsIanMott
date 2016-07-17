a4ff0f0a4ff7c9ec43273910cb7a61425e123137
ianmott <ian@derivedalgorithm.com>
2016-07-16 20:14:55 -0400

    u

--- main.js
+++ main.js
@@ -19,11 +19,14 @@ module.exports.loop = function() {
 	var NumofTypes = 5;
 	var toggle = false;
     var Types = new Array(NumofTypes);           // Type Name     Type Min       Type Current
-    Types[0] = new Array(3); Types[0][0] = 'harvester'; Types[0][1] = 3; Types[0][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester').length;
+    Types[0] = new Array(3); Types[0][0] = 'harvester'; Types[0][1] = 5; Types[0][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester').length;
     Types[1] = new Array(3); Types[1][0] = 'builder';   Types[1][1] = 2; Types[1][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder').length;
-    Types[2] = new Array(3); Types[2][0] = 'upgrader';  Types[2][1] = 6; Types[2][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader').length;
-    Types[3] = new Array(3); Types[3][0] = 'repairer';  Types[3][1] = 4; Types[3][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer').length;
-    Types[4] = new Array(3); Types[4][0] = 'guard';  Types[4][1] = 2; Types[4][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'guard').length;
+    Types[2] = new Array(3); Types[2][0] = 'upgrader';  Types[2][1] = 5; Types[2][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader').length;
+    Types[3] = new Array(3); Types[3][0] = 'repairer';  Types[3][1] = 5; Types[3][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer').length;
+    Types[4] = new Array(3); Types[4][0] = 'guard';  Types[4][1] = 1; Types[4][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'guard').length;
+    
+    if (Game.time%10==0){ console.log('Energy:'+Game.spawns.Spawn1.room.energyAvailable+' h#/X='+Types[0][2]+'/'+Types[0][1]+' b#/X='+Types[1][2]+'/'+Types[1][1]+' u#/X='+Types[2][2]+'/'+Types[2][1]+' r#/X='+Types[3][2]+'/'+Types[3][1]+' g#/X='+Types[4][2]+'/'+Types[4][1]);
+    }
     
     for (i=0; i <NumofTypes; i++){
         if (Types[i][1] > Types[i][2] && (Game.spawns.Spawn1.room.energyAvailable) > energyspawnlevel && !toggle)
@@ -55,8 +58,6 @@ module.exports.loop = function() {
         if (creep.memory.role == 'guard'){
             roleguard.run(creep);
         }
-    }
-    if (Game.time%10==0){ console.log('Energy:'+Game.spawns.Spawn1.room.energyAvailable+' h#/X='+Types[0][2]+'/'+Types[0][1]+' b#/X='+Types[1][2]+'/'+Types[1][1]+' u#/X='+Types[2][2]+'/'+Types[2][1]+' r#/X='+Types[3][2]+'/'+Types[3][1]+' g#/X='+Types[4][2]+'/'+Types[4][1]);
     }
     //console.log(Game.time);
 };
