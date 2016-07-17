9880d93018bbb52569b67e63d4b373805846c178
ianmott <ian@derivedalgorithm.com>
2016-07-16 22:16:08 -0400

    u

--- main.js
+++ main.js
@@ -19,24 +19,19 @@ module.exports.loop = function() {
 	var NumofTypes = 5;
 	var toggle = false;
     var Types = new Array(NumofTypes);           // Type Name     Type Min       Type Current
-    Types[0] = new Array(3); Types[0][0] = 'harvester'; Types[0][1] = 5; Types[0][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester').length;
-    Types[1] = new Array(3); Types[1][0] = 'builder';   Types[1][1] = 2; Types[1][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder').length;
-    Types[2] = new Array(3); Types[2][0] = 'upgrader';  Types[2][1] = 5; Types[2][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader').length;
-    Types[3] = new Array(3); Types[3][0] = 'repairer';  Types[3][1] = 5; Types[3][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer').length;
-    Types[4] = new Array(3); Types[4][0] = 'guard';  Types[4][1] = 1; Types[4][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'guard').length;
+    Types[0] = new Array(4); Types[0][0] = 'harvester'; Types[0][1] = 5; Types[0][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester').length;   Types[0][3] = [WORK,WORK,CARRY,MOVE];
+    Types[1] = new Array(4); Types[1][0] = 'builder';   Types[1][1] = 4; Types[1][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder').length;     Types[1][3] = [WORK,WORK,CARRY,MOVE];
+    Types[2] = new Array(4); Types[2][0] = 'upgrader';  Types[2][1] = 8; Types[2][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader').length;    Types[2][3] = [WORK,WORK,CARRY,MOVE];
+    Types[3] = new Array(4); Types[3][0] = 'repairer';  Types[3][1] = 5; Types[3][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer').length;    Types[3][3] = [WORK,WORK,CARRY,MOVE];
+    Types[4] = new Array(4); Types[4][0] = 'guard';     Types[4][1] = 2; Types[4][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'guard').length;       Types[4][3] = [TOUGH,ATTACK,ATTACK,MOVE];
     
     if (Game.time%10==0){ console.log('Energy:'+Game.spawns.Spawn1.room.energyAvailable+' h#/X='+Types[0][2]+'/'+Types[0][1]+' b#/X='+Types[1][2]+'/'+Types[1][1]+' u#/X='+Types[2][2]+'/'+Types[2][1]+' r#/X='+Types[3][2]+'/'+Types[3][1]+' g#/X='+Types[4][2]+'/'+Types[4][1]);
     }
     
     for (i=0; i <NumofTypes; i++){
         if (Types[i][1] > Types[i][2] && (Game.spawns.Spawn1.room.energyAvailable) > energyspawnlevel && !toggle)
-        {
-            if (Types[i][0] == 'guard'){ 
-                var newName = Game.spawns.Spawn1.createCreep([TOUGH,ATTACK,ATTACK,MOVE], undefined, {role: Types[i][0]});
-            }
-            else {             
-                var newName = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: Types[i][0]});
-            }
+        {         
+            var newName = Game.spawns.Spawn1.createCreep(Types[i][3], undefined, {role: Types[i][0]});
             console.log('Spawning new '+Types[i][0]+': ' + newName);
             toggle = true;
         }
