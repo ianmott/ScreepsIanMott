5bdeea2783c1621c57ffe6b1c347f21d86e63431
ianmott <ian@derivedalgorithm.com>
2016-07-19 23:22:31 -0400

    Added Weights to adjust spawn order

--- main.js
+++ main.js
@@ -18,30 +18,40 @@ module.exports.loop = function() {
 	
 	// Variables
 	var NumofTypes = 8;
+	var numofFields = 5;
+	var TWeights = new Array(NumofTypes);
+	   //      Harvester            Guard         Upgrader         repairer        
+	    TWeights[0] = 0; TWeights[1] = 1; TWeights[2] = 4; TWeights[3] = 5;
+	   //      builder       rangedguard        tharvester      atcontroller
+	    TWeights[4] = 6; TWeights[5] = 3; TWeights[6] = 2; TWeights[7] = 7;
 	var toggle = false;
     var Types = new Array(NumofTypes);           // Type Name     Type Min       Type Current
         // Parts                                                        Cost
-    Types[0] = new Array(5); Types[0][0] = 'harvester'; Types[0][1] = 3; Types[0][4] = 1199; Types[0][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester').length;   
-        Types[0][3] = [WORK,WORK,CARRY,CARRY,MOVE,CARRY,WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE];  
-    Types[1] = new Array(5); Types[1][0] = 'guard';     Types[1][1] = 1; Types[1][4] = 1199; Types[1][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'guard').length; 
-        Types[1][3] = [TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,MOVE,MOVE,ATTACK,MOVE,MOVE]; 
-    Types[2] = new Array(5); Types[2][0] = 'upgrader';  Types[2][1] = 0; Types[2][4] = 1199; Types[2][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader').length;   
-        Types[2][3] = [WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,CARRY,WORK,WORK,MOVE,CARRY,CARRY,MOVE,MOVE]; 
-    Types[3] = new Array(5); Types[3][0] = 'repairer';  Types[3][1] = 0;  Types[3][4] = 1199; Types[3][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer').length;    
-        Types[3][3] = [CARRY,WORK,WORK,WORK,CARRY,CARRY,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE]; 
-    Types[4] = new Array(5); Types[4][0] = 'builder';   Types[4][1] = 0;  Types[4][4] = 1199; Types[4][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder').length;     
-        Types[4][3] = [WORK,WORK,WORK,WORK,CARRY,CARRY,WORK,WORK,WORK,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE];  
-    Types[5] = new Array(5); Types[5][0] = 'rangedguard';Types[5][1] = 1; Types[5][4] = 1199; Types[5][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'rangedguard').length; 
-        Types[5][3] = [TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,MOVE,MOVE,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK];
-    Types[6] = new Array(5); Types[6][0] = 'tharvester';Types[6][1] = 1;  Types[6][4] = 999; Types[6][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'tharvester').length; 
-        Types[6][3] = [WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE];
-    Types[7] = new Array(5); Types[7][0] = 'atcontroller';Types[7][1] = 0; Types[7][4] = 849; Types[7][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'atcontroller').length; 
-        Types[7][3] = [CLAIM,MOVE,MOVE,MOVE,MOVE,MOVE];
+    Types[TWeights[0]] = new Array(numofFields); Types[TWeights[0]][0] = 'harvester'; Types[TWeights[0]][1] = 3; Types[TWeights[0]][4] = 1199; Types[TWeights[0]][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester').length;   
+        Types[TWeights[0]][3] = [WORK,WORK,CARRY,CARRY,MOVE,CARRY,WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE];  
+    Types[TWeights[1]] = new Array(numofFields); Types[TWeights[1]][0] = 'guard';     Types[TWeights[1]][1] = 1; Types[TWeights[1]][4] = 1199; Types[TWeights[1]][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'guard').length; 
+        Types[TWeights[1]][3] = [TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,MOVE,MOVE,ATTACK,MOVE,MOVE]; 
+    Types[TWeights[2]] = new Array(numofFields); Types[TWeights[2]][0] = 'upgrader';  Types[TWeights[2]][1] = 5; Types[TWeights[2]][4] = 1199; Types[TWeights[2]][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader').length;   
+        Types[TWeights[2]][3] = [WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,CARRY,WORK,WORK,MOVE,CARRY,CARRY,MOVE,MOVE]; 
+    Types[TWeights[3]] = new Array(numofFields); Types[TWeights[3]][0] = 'repairer';  Types[TWeights[3]][1] = 3;  Types[TWeights[3]][4] = 1199; Types[TWeights[3]][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer').length;    
+        Types[TWeights[3]][3] = [CARRY,WORK,WORK,WORK,CARRY,CARRY,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE]; 
+    Types[TWeights[4]] = new Array(numofFields); Types[TWeights[4]][0] = 'builder';   Types[TWeights[4]][1] = 1;  Types[TWeights[4]][4] = 1199; Types[TWeights[4]][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder').length;     
+        Types[TWeights[4]][3] = [WORK,WORK,WORK,WORK,CARRY,CARRY,WORK,WORK,WORK,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE];  
+    Types[TWeights[5]] = new Array(numofFields); Types[TWeights[5]][0] = 'rangedguard';Types[TWeights[5]][1] = 2; Types[TWeights[5]][4] = 1199; Types[TWeights[5]][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'rangedguard').length; 
+        Types[TWeights[5]][3] = [TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,MOVE,MOVE,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK];
+    Types[TWeights[6]] = new Array(numofFields); Types[TWeights[6]][0] = 'tharvester';Types[TWeights[6]][1] = 1;  Types[TWeights[6]][4] = 999; Types[TWeights[6]][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'tharvester').length; 
+        Types[TWeights[6]][3] = [WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE];
+    Types[TWeights[7]] = new Array(numofFields); Types[TWeights[7]][0] = 'atcontroller';Types[TWeights[7]][1] = 0; Types[TWeights[7]][4] = 849; Types[TWeights[7]][2] = _.filter(Game.creeps, (creep) => creep.memory.role == 'atcontroller').length; 
+        Types[TWeights[7]][3] = [CLAIM,MOVE,MOVE,MOVE,MOVE,MOVE];
     
     if (Game.time%10<1){ 
-        console.log('Energy:'+Game.spawns.Spawn1.room.energyAvailable+' h='+Types[0][2]+'/'+Types[0][1]+' th='+Types[6][2]+'/'+Types[6][1]+' b='+Types[4][2]+'/'+Types[4][1]+' u='+Types[2][2]+'/'+Types[2][1]+' r='+Types[3][2]+'/'+Types[3][1]+' g='+Types[1][2]+'/'+Types[1][1]+' rg='+Types[5][2]+'/'+Types[5][1]+' at='+Types[7][2]+'/'+Types[7][1]);
+        var stringbuilder = 'Energy:'+Game.spawns.Spawn1.room.energyAvailable;
+        for (i = 0; i< NumofTypes;i++) {
+        stringbuilder += ' '+Types[TWeights[i]][0]+' #'+TWeights[i]+' '+Types[TWeights[i]][2]+'/'+Types[TWeights[i]][1];
+        }
+        console.log(stringbuilder);
     }
-    if (Types[0][2] == 1){
+    if (Types[0][2] <= 1){
         if (Game.spawns.Spawn1.room.energyAvailable > 999){
             var newName = Game.spawns.Spawn1.createCreep([WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE], undefined, {role: Types[0][0]});
             console.log('Spawning new '+Types[0][0]+': ' + newName);
